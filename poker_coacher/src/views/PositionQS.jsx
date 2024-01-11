092aa03970fb6785583ea18113cec58ccff79bb2
Nir Cohen <nir@laviby.co.il>
2024-01-11 15:57:57 +0200

    question types

---
+++ poker_coacher/src/views/PositionQS.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PositionQS() {
+  return <div>PositionQS</div>;
+}
+
+export default PositionQS;
---
+++ poker_coacher/src/views/TableQS.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TableQS() {
+  return <div>Table</div>;
+}
+
+export default TableQS;
