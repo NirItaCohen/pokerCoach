97c9a5b172299c3e909c5524460ed7f13f538dca
Nir Cohen <nir.cohen.a30@gmail.com>
2024-01-31 10:25:00 +0200

    Co-authored-by: Oren Damien Shomrai <damien.shomrai@pm.me>

--- poker_coacher/src/components/Question.jsx
+++ poker_coacher/src/components/Question.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
+import React from "react";
+import EquityQS from "../views/EquityQS";
+import As from "@assets/cards/Kd.svg";
+import { Suspense, useEffect, useState } from "react";
 
-function Question({data}) {
+function Question({ data }) {
+  const [svg, setSvg] = useState("");
 
-  const renderQS = () => {
-    
-  }
+  useEffect(() => {
+    const loadImg = async () => {
+      const cardImgPath = await getCardImage(data.question.hand1[0]);
+      setSvg(cardImgPath);
+    };
+
+    loadImg();
+  }, []);
+
+  const getCardImage = async (cardName) => {
+    const path = `../assets/cards/${cardName}.svg`;
+    const importedImg = await import(path);
+    console.log(importedImg);
+
+    return importedImg.default;
+  };
+
+  const getLazyCardImage = async (cardName) => {
+    const path = `../assets/cards/${cardName}.svg`;
+    const importedImg = React.lazy(() => import(path).default);
+    console.log(importedImg);
+
+    return importedImg;
+  };
+
+  console.log(svg);
+
   return (
     <>
-      
+      <Suspense fallback={<div>Loading...</div>}>
+        <img src={svg} />
+      </Suspense>
+      <img src={As} />
+
+      <img src={svg} />
     </>
-  )
+  );
 }
 
-export default Question
+export default Question;
--- poker_coacher/src/pages/Trainer.jsx
+++ poker_coacher/src/pages/Trainer.jsx
@@ -3,14 +3,17 @@ import questions from "../assets/qs/questions.json";
 import Question from "../components/Question";
 function Trainer() {
   const [qs, setQs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setQs(questions);
+    setQs(questions.questions);
+    setIsLoading(false);
   }, []);
 
   return (
-    <div>
-      <Question data={questions} />
+    <div className="bg-slate-200 w-75 h-full">
+      <h3></h3>
+      {!isLoading ? <Question data={qs[0]} /> : <h3>loading...</h3>}
     </div>
   );
 }
